4addcc3a28ba4962f1e788e24a579ce01f07f931
Bartek <bartekkania98@gmail.com>
2026-02-08 10:39:02 +0100

    Dodanie clocale Polish. Rozroznianie polskich znakow

--- Gra/Project1/main.cpp
+++ Gra/Project1/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <clocale>
 
 int main() {
+    setlocale(LC_CTYPE, "Polish");
     std::cout << "Czeœæ, chcia³bym stworzyæ szachy" << std::endl;
     std::cin.get(); // Czeka na wciœniêcie klawisza
     return 0;
