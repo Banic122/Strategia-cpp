823a6514641660c18a05b24442f7d268970a0be8
Bartek <bartekkania98@gmail.com>
2026-02-08 09:59:36 +0100

    Tworzenie gry

--- Gra/Project1/main.cpp
+++ Gra/Project1/main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Raport: Gotowy do nauki C++!" << std::endl;
+    std::cout << "Czeœæ, chcia³bym stworzyæ szachy" << std::endl;
     std::cin.get(); // Czeka na wciœniêcie klawisza
     return 0;
 }
